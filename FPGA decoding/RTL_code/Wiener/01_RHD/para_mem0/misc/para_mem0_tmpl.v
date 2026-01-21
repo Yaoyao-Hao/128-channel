5068ab7864bdf42777024bc586a72247cf2083ec
YCB-1027 <83754757+YCB-1027@users.noreply.github.com>
2026-01-21 17:30:12 +0800

    Delete FPGA decoding/RTL_code/Wiener/01_RHD directory

--- FPGA decoding/RTL_code/Wiener/01_RHD/para_mem0/misc/para_mem0_tmpl.v
+++
@@ -1,7 +0,0 @@
-    para_mem0 __(.clk_i( ),
-        .rst_i( ),
-        .clk_en_i( ),
-        .wr_en_i( ),
-        .wr_data_i( ),
-        .addr_i( ),
-        .rd_data_o( ));
--- FPGA decoding/RTL_code/Wiener/01_RHD/para_mem0/testbench/dut_inst.v
+++
@@ -1,7 +0,0 @@
-    para_mem0 u_para_mem0(.clk_i(clk_i),
-        .rst_i(rst_i),
-        .clk_en_i(clk_en_i),
-        .wr_en_i(wr_en_i),
-        .wr_data_i(wr_data_i),
-        .addr_i(addr_i),
-        .rd_data_o(rd_data_o));
